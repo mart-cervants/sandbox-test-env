26ec618655c2792d6fa34f9818ce9095e75abcd7
martin_cervantes <martin_cervantes@epam.com>
2023-12-03 16:57:58 +0000

    create medallion tables task removed

--- dags/dataproc_workflow_dag.py
+++ dags/dataproc_workflow_dag.py
@@ -2,29 +2,27 @@ import datetime
 import os
 
 from airflow import models
+from pathlib import Path
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.operators.dataproc import (
-    DataprocCreateClusterOperator,
-    DataprocDeleteClusterOperator,
-    DataprocSubmitJobOperator,
+    DataprocSubmitJobOperator
 )
 
-# ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "dataproc_pyspark"
-# PROJECT_ID = os.environ.get("SYSTEM_TESTS_GCP_PROJECT")
-PROJECT_ID = "mcervantes-trainings"
-JOB_FILE_URI = "gs://scripts-data-sandbox/medallion-tables-creation/create_medallion_comments_tables.py"
-# CLUSTER_NAME = f"cluster-{ENV_ID}-{DAG_ID}".replace("_", "-")
+PROJECT_ID = os.environ.get("SYSTEM_TESTS_GCP_PROJECT")
+INSERT_DATA_JOB_URI = "gs://scripts-data-sandbox/medallion-tables-data-insertion/insert_data_into_medallion_comment_tables.py"
 CLUSTER_NAME = "data-sandbox"
 REGION = "us-central1"
+
 
 default_dag_args = {
     "start_date": datetime.datetime(2021, 1, 1),
 }
 
-PYSPARK_JOB = {
+INSERT_DATA_PYSPARK_JOB = {
     "reference": {"project_id": PROJECT_ID},
     "placement": {"cluster_name": CLUSTER_NAME},
-    "pyspark_job": {"main_python_file_uri": JOB_FILE_URI},
+    "pyspark_job": {"main_python_file_uri": INSERT_DATA_JOB_URI},
 }
 
 with models.DAG(
@@ -33,15 +31,9 @@ with models.DAG(
     default_args=default_dag_args,
 ) as dag:
 
-    create_medallion_comments_tables = DataprocSubmitJobOperator(
-        task_id="create_medallion_comments_tables", job=PYSPARK_JOB, region=REGION, project_id=PROJECT_ID
+    insert_data_medallion_comments_tables = DataprocSubmitJobOperator(
+        task_id="insert_data_medallion_comments_tables", job=INSERT_DATA_PYSPARK_JOB, region=REGION, project_id=PROJECT_ID
     )
 
-    # # TEST SETUP
-    # create_cluster
-    # # TEST BODY
-    # >> pyspark_task
-    # # TEST TEARDOWN
-    # >> delete_cluster
-
-    create_medallion_comments_tables
+    insert_data_medallion_comments_tables
+    
